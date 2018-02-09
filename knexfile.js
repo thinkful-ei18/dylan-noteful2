246bfdddcf97e908ea20fc5e175642a817060694
Dylan <nielsen.dylan@gmail.com>
2018-02-09 13:54:10 -0800

    Will this break it?

--- knexfile.js
+++ knexfile.js
@@ -11,10 +11,10 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgres://localhost/noteful_test',
     debug: true,
-    pool: {min: 1, max: 2}
+    pool: { min: 1, max: 2 }
   },
   production: {
     client: 'pg',
-    connection: 'postgres://mkrlncgqmdzvxz:033d6c172db8271075fa757c9e144408ac15cea944419880955837167e7e13ec@ec2-54-243-59-122.compute-1.amazonaws.com:5432/d3qrhfkikrn8pn'
+    connection: process.env.DATABASE_URL
   }
 };
